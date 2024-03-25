21bd0e9a9371ac75f772f1a8494f2b569195b92e
Dario Espinola <despinola91@gmail.com>
2024-03-24 23:16:51 -0300

    Agregado inicial de clase de negocio

---
+++ src/game_2048/Negocio.java
@@ -0,0 +1,55 @@
+package game_2048;
+
+public class Negocio {
+	
+	private int [][] matriz = new int[4][4];
+	
+	private enum accion {
+		Arriba,
+		Abajo,
+		Izquierda,
+		Derecha
+	}
+	
+	public void IniciarMatriz() {
+		int fila = ObtenerFilaRandom();
+		int columna = ObtenerColumnaRandom();
+		
+		matriz[fila][columna] = ObtenerValorRandom();
+	}
+	
+	private int ObtenerValorRandom() {
+		int valor = 2; //Debe ser 2 o 4.
+		return valor;
+	}
+	
+	private int ObtenerFilaRandom() {
+		//Debe devolver una posición para asignar el número random. La posición debe ser una disponible luego del reordenamiento.
+		int valor = 1;
+		return valor;
+	}
+	
+	private int ObtenerColumnaRandom() {
+		//Debe devolver una posición para asignar el número random. La posición debe ser una disponible luego del reordenamiento.
+		int valor = 1;
+		return valor;
+	}
+	
+	public void ReordenarMatriz (accion AccionUsuario) {
+		switch(AccionUsuario) {
+			case accion.Arriba:
+				break;
+			case accion.Abajo:
+				break;
+			case accion.Izquierda:
+				break;
+			case accion.Derecha:
+				break;
+		}
+	}
+	
+	public int[][] ObtenerMatriz() {
+		return matriz;
+	}
+
+}
